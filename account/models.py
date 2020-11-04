a3c23cfbc3d35915c842cce928ead3ce75845eeb
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-11-04 16:57:36 +0100

    Refactor

--- account/models.py
+++ account/models.py
@@ -37,7 +37,6 @@ class User(AbstractUser):
     gender = models.CharField(max_length=1, choices=GENDER)
     profile_pic = models.ImageField(null=False)
     address = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
