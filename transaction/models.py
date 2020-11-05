1f756e5dd0e0120c0a20b077b49c97d50565f7f4
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-11-05 12:55:14 +0100

    Transaction model

--- transaction/models.py
+++ transaction/models.py
@@ -3,14 +3,6 @@ from account.models import Customer
 # Create your models here.
 
 
-class Debit(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL)
-    description = models.CharField(max_length=100)
-    balance_before = models.FloatField()
-    balance = models.FloatField()
-    created_at = models.DateTimeField(auto_now=True)
-
-
 class Transaction(models.Model):
     TYPE = [('Debit', 'Debit'), ('Credit', 'Credit')]
     receiver = models.ForeignKey(Customer, on_delete=models.SET_NULL)
