0e13200cff311f3656031827ff1aa337ba900e1f
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-11-12 12:28:00 +0100

    Changed Transaction

--- transaction/models.py
+++ transaction/models.py
@@ -3,17 +3,13 @@ from account.models import Customer
 # Create your models here.
 
 
-class Transaction(models.Model):
-    TYPE = [('Debit', 'Debit'), ('Credit', 'Credit')]
-    sender = models.ForeignKey(
-        Customer, on_delete=models.CASCADE, related_name="sender")
-    receiver = models.ForeignKey(
+class BankCreditTransaction(models.Model):
+    customer = models.ForeignKey(
         Customer, on_delete=models.CASCADE, related_name="receiver")
     description = models.CharField(max_length=100)
     balance_before = models.FloatField()
     amount = models.FloatField()
     status = models.SmallIntegerField(
         default=0, help_text="If 0, transaction is pending; approved if 1, rejected if -1")
-    category = models.CharField(max_length=6, choices=TYPE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
