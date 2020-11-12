03068036dffeacd57d23dcece1c0876c8a55e8ae
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-11-12 21:33:29 +0100

    Edited models

--- transaction/models.py
+++ transaction/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from account.models import User
 from django.core.validators import MinValueValidator
 
 # Create your models here.
