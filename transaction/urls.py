759574eed5b11fbce9d74c59cc7e88d693b0587d
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-11-16 13:04:56 +0100

    Here

--- transaction/urls.py
+++ transaction/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('view/<int:customer_id>/',
          views.view_transaction, name='view_transaction'),
     path('debit/', views.debit_transaction, name='debit_transaction'),
+    path('transfer/', views.debit_transaction, name='transfer_transaction'),
     path('verify/<int:transaction_id>/',
          views.verify_transaction, name='verify_transaction'),
 ]
